4b09974a31e530f595dfd27139210e746b12afa4
dukia-gold-tech <it@dukiagoldrefinery.co>
2026-02-18 22:32:55 +0100

    Initial commit for MEMRA coming soon page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <img
+          src="/memra_logo.png"
+          alt="MEMRA Logo"
+          className="logo"
+        />
+        <h1 className="heading">Coming Soon!</h1>
+        <p className="subheading">We're working hard to bring MEMRA to life.</p>
+        <p className="description"> Stay tuned!</p>
+      </div>
     </div>
   );
 }
